2e86a1ca3a92f8855c216f49c135c58771289147
BrunaDev <dev.bruna.magalhaes@gmail.com>
2026-05-29 23:32:03 -0300

    fix(ui): importa CSS do react-slick para corrigir renderização do carrossel

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/home";
